1ccfcdbffcc43e946f383ec3c7bd65aae102b2e6
J1NU2 <sdrtst@naver.com>
2022-05-19 21:31:20 +0900

    토큰에 따른 로그아웃/로그인 버튼 전환

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ async function loadArticles() {
     const article_list = document.getElementById("articles")
 
     articles.forEach(article => {
-        console.log(article)
         const newArticle = document.createElement("li");
         newArticle.setAttribute("id", article._id)
         newArticle.innerText = article.title
+        newArticle.setAttribute("onclick", "articleDetail(this.id)")
         article_list.appendChild(newArticle)
     });
 }
 
+async function checkLogin() {
+    const name = await getName();
+    console.log(name)
+    const username = document.getElementById("username")
+    const loginoutButton = document.getElementById("loginout")
+    if (name) {
+        username.innerText = name
+        loginoutButton.innerText = "로그아웃"
+        loginoutButton.setAttribute("onclick", "logout()")
+    } else {
+        username.innerText = "로그인이 필요합니다."
+        loginoutButton.innerText = "로그인"
+        loginoutButton.setAttribute("onclick", "location.href='/login.html'")
+    }
+}
+
+checkLogin();
 loadArticles();
-getName();
